12bd671720973f118593a342072a887d18b45e20
prashanth103 <cheeralaprashanth10@gmail.com>
2026-07-09 10:30:18 +0530

    font manrope

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,18 @@
-﻿import { Stack } from 'expo-router';
+﻿import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    ManropeRegular: require('../assets/fonts/Manrope-Regular.ttf'),
+    ManropeMedium: require('../assets/fonts/Manrope-Medium.ttf'),
+    ManropeSemiBold: require('../assets/fonts/Manrope-SemiBold.ttf'),
+    ManropeBold: require('../assets/fonts/Manrope-Bold.ttf'),
+    ManropeExtraBold: require('../assets/fonts/Manrope-ExtraBold.ttf'),
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <>
       <StatusBar style="light" />
